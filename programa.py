dd47dcee6e0b11ae5c9fcad364dc9088583cf57d
Maira Gentil <mairajgs@al.insper.edu.br>
2024-10-27 22:45:16 -0300

    Pergunta os ataques do jogador

--- programa.py
+++ programa.py
@@ -1,4 +1,6 @@
 from funcoes import *
+import random 
+random.seed(2)
 
 nom= {"porta-aviões":[4,1], "navio-tanque":[3,2], "contratorpedeiro":[2,3], "submarino":[1,4]}
 # [tamanho,quantidade]
@@ -14,7 +16,7 @@ for nome,(tamanho,qnt) in nom.items():
             orientacao = int(input("[1] Vertical [2] Horizontal >"))
             if orientacao == 1:
                 orientacao = 'vertical'
-                
+
             elif orientacao == 2:
                 orientacao = 'horizontal'
 
@@ -39,4 +41,75 @@ for nome,(tamanho,qnt) in nom.items():
 
         p_definidas = define_posicoes(linha, coluna, orientacao, tamanho)
         prefrota = preenche_frota(frotav,nome,linha,coluna,orientacao,tamanho)
-print(frotav)
+# print(frotav)
+
+frota_oponente = {
+    'porta-aviões': [
+        [[9, 1], [9, 2], [9, 3], [9, 4]]
+    ],
+    'navio-tanque': [
+        [[6, 0], [6, 1], [6, 2]],
+        [[4, 3], [5, 3], [6, 3]]
+    ],
+    'contratorpedeiro': [
+        [[1, 6], [1, 7]],
+        [[0, 5], [1, 5]],
+        [[3, 6], [3, 7]]
+    ],
+    'submarino': [
+        [[2, 7]],
+        [[0, 6]],
+        [[9, 7]],
+        [[7, 6]]
+    ]
+}
+
+taboponente = posiciona_frota(frota_oponente)
+tabjogador = posiciona_frota(frotav)
+
+print(monta_tabuleiros(tabjogador,taboponente))
+
+jogando = True
+jogadas_feitas = []
+jogadas_oponentes = []
+
+while jogando == True:
+    valida = - 1
+    while valida == -1:
+        linha = int(input('Jogador, qual linha deseja atacar? '))
+        quebra = 0
+        if linha < 0 or linha > 9:
+            print('Linha inválida!')
+            quebra = -1
+        while quebra == -1:
+            linha = int(input('Jogador, qual linha deseja atacar? '))
+            quebra = 0
+            if linha < 0 or linha > 9:
+                print('Linha inválida!')
+                quebra = -1
+        
+        coluna = int(input('Jogador, qual coluna deseja atacar? '))
+        quebra = 0
+        if coluna < 0 or coluna > 9:
+            print('Coluna inválida!')
+            quebra = -1
+        while quebra == -1:
+            coluna = int(input('Jogador, qual coluna deseja atacar? '))
+            quebra = 0
+            if coluna < 0 or coluna > 9:
+                print('Coluna inválida!')
+                quebra = -1
+        jogado = [linha,coluna]
+        if jogado not in jogadas_feitas:
+            jogadas_feitas.append(jogado)
+            valida = 0
+        else:
+            print(f'A posição linha {linha} e coluna {coluna} já foi informada anteriormente!')
+
+    taboponente = faz_jogada(taboponente,linha,coluna)
+    quantidade_afundados = afundados(frota_oponente,taboponente)
+
+    if quantidade_afundados == 10:
+        print('Parabéns! Você derrubou todos os navios do seu oponente!')
+        jogando = False
+
